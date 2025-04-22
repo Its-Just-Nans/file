6ef107a1ffe8b2cd6a51b585c358614805edebc0
n4n5 <its.just.n4n5@gmail.com>
2025-04-22 16:53:51 +0200

    add exif

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ struct FileFormatDetected {
     short_name: String,
     media_type: String,
     extension: String,
+    is_img: bool,
+    exif_data: Vec<String>,
 }
 
 #[derive(Serialize)]
@@ -19,10 +21,32 @@ struct Output {
     file_type: FileFormatDetected,
 }
 
+fn get_exif(data: &[u8]) -> Vec<String> {
+    let exifreader = exif::Reader::new();
+    let mut reader = std::io::Cursor::new(data);
+    let exif = exifreader.read_from_container(&mut reader);
+    let exif = match exif {
+        Ok(exif) => exif,
+        Err(_) => return Vec::new(),
+    };
+    let mut exif_fields = Vec::new();
+    for f in exif.fields() {
+        exif_fields.push(format!(
+            "{} {} {}",
+            f.tag,
+            f.ifd_num,
+            f.display_value().with_unit(&exif)
+        ));
+    }
+    exif_fields
+}
+
 #[wasm_bindgen]
 pub fn process_file(data: &[u8]) -> String {
     let file_len = data.len();
     let fmt = FileFormat::from_bytes(data);
+    let is_img = fmt.kind() == file_format::Kind::Image;
+    let exif_data = if is_img { get_exif(data) } else { Vec::new() };
     let output = Output {
         file_len,
         file_type: FileFormatDetected {
@@ -34,6 +58,8 @@ pub fn process_file(data: &[u8]) -> String {
                 .unwrap_or("".to_string()),
             media_type: fmt.media_type().to_string(),
             extension: fmt.extension().to_string(),
+            is_img,
+            exif_data,
         },
     };
     match serde_json::to_string(&output) {
